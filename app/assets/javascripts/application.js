303fbaae954b9b66fd0fbbad98087b74be2e5fac
Virginie <virginie.lenoir17@gmail.com>
2020-06-10 13:55:19 +0200

    add beggining home page, routes to homepage and navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require activestorage
-//= require turbolink
-//= require theme.min
 //= require_tree .
+//= require rails-ujs
+//= require turbolinks
+//= require activestorage
+//= require theme.min
+//= require staticPages
