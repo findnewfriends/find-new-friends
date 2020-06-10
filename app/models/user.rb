1964ca198303e01dcb13be86ab16d67331c9aacd
Tomaz <tmedrado99@gmail.com>
2020-06-10 09:34:27 -0300

    devise setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
-  belongs_to :city
-    belongs_to :job
+  belongs_to :city, optional: true
+    belongs_to :job , optional: true
 
     has_many :feedbacks
     has_many :flags
