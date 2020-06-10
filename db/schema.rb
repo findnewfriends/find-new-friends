eca1f7c9f7694ad8c7def8ae3141d8fe36c09e65
Virginie <virginie.lenoir17@gmail.com>
2020-06-10 22:06:36 +0200

    changes to navbar sign out button

--- db/schema.rb
+++ db/schema.rb
@@ -22,25 +22,24 @@ ActiveRecord::Schema.define(version: 2020_06_10_111655) do
   end
 
   create_table "feedbacks", force: :cascade do |t|
-    t.bigint "user_id"
-    t.integer "score"
+    t.bigint "author_id"
+    t.bigint "recipient_id"
     t.text "content"
     t.boolean "recommended"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "author_id"
     t.index ["author_id"], name: "index_feedbacks_on_author_id"
-    t.index ["user_id"], name: "index_feedbacks_on_user_id"
+    t.index ["recipient_id"], name: "index_feedbacks_on_recipient_id"
   end
 
   create_table "flags", force: :cascade do |t|
-    t.bigint "user_id"
+    t.bigint "author_id"
+    t.bigint "recipient_id"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "author_id"
     t.index ["author_id"], name: "index_flags_on_author_id"
-    t.index ["user_id"], name: "index_flags_on_user_id"
+    t.index ["recipient_id"], name: "index_flags_on_recipient_id"
   end
 
   create_table "interests", force: :cascade do |t|
@@ -67,24 +66,26 @@ ActiveRecord::Schema.define(version: 2020_06_10_111655) do
 
   create_table "matches", force: :cascade do |t|
     t.bigint "user_id"
-    t.integer "score"
-    t.integer "accepted"
+    t.bigint "matched_user_id"
+    t.float "score"
+    t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "match_user_id"
-    t.index ["match_user_id"], name: "index_matches_on_match_user_id"
+    t.index ["matched_user_id"], name: "index_matches_on_matched_user_id"
     t.index ["user_id"], name: "index_matches_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "email"
     t.text "description"
     t.date "birthdate"
+    t.string "gender"
     t.bigint "job_id"
     t.bigint "city_id"
-    t.string "gender"
+    t.string "role", default: "user", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "reset_password_token"
@@ -101,11 +102,8 @@ ActiveRecord::Schema.define(version: 2020_06_10_111655) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "feedbacks", "users"
-  add_foreign_key "flags", "users"
   add_foreign_key "join_user_interests", "interests"
   add_foreign_key "join_user_interests", "users"
-  add_foreign_key "matches", "users"
   add_foreign_key "users", "cities"
   add_foreign_key "users", "jobs"
 end
