5871c0da5ed793cc24df40613c7bd8f57818202f
antu <katehuen@gmail.com>
2020-06-12 17:07:35 +0200

    Algo on matching_score.rb service version 0

--- app/services/matching_score.rb
+++ app/services/matching_score.rb
@@ -1,7 +1,11 @@
 class MatchingScore
   def perform(user1,user2)
-    
-
+    interests1 = JoinUserInterest.where(user:user1).collect(&:interest)
+    interests2 = JoinUserInterest.where(user:user2).collect(&:interest)
+    puts "================================"
+    puts interests1
+    puts interests2
+    puts "================================"
 
     return rand(100..200)
   end
