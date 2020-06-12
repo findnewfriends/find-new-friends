340a6dd14931795075bf605dada2eabe228c151a
Virginie <virginie.lenoir17@gmail.com>
2020-06-12 22:21:40 +0200

    changes to profile page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   root :to => 'static_pages#home'
   resources :matches, only: [:show, :index]
-  resources :users, only: [:index]
+  resources :users, only: [:index, :show]
 end
