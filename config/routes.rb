36bb50e853ee44227031c1d6d959c9dade6cd607
antu <katehuen@gmail.com>
2020-06-12 23:51:40 +0200

    modified routes again

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   default_url_options :host => "thp-find-new-friends.herokuapp.com"
-  devise_for :users
-  # devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root :to => 'static_pages#home'
   resources :matches, only: [:show, :index]
   resources :users, only: [:index, :show]
