f4fe90c64e729564392057e268c50df078db6bb8
antu <katehuen@gmail.com>
2020-06-13 14:54:12 +0200

    added missing ! to authenticate_user! definition

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,13 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!, :only => [:index]
 
-  def index 
-        # on code quelque chose qui permet d'afficher le dashboard de l'utilisateur 
+  def index
+        # on code quelque chose qui permet d'afficher le dashboard de l'utilisateur
   end
 
   private
 
-  def authenticate_user
+  def authenticate_user!
     unless current_user
       flash[:danger] = "Please log in."
       redirect_to new_session_path
