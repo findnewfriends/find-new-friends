3d8e4eeacbb8dea2616ef2f6f55c3c51c4d6c448
antu <katehuen@gmail.com>
2020-06-15 16:23:18 +0200

    added send request to matches index, plus show in view when request has been sent or recevied already

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -13,4 +13,37 @@ class MatchesController < ApplicationController
     # end
   end
 
+
+  def update
+    puts "---------------------------------------------o-o-o-o-o---o-o-o-o-o- params are #{params}"
+    puts "---------------------------------------------o-o-o-o-o---o-o-o-o-o- match_params are #{match_params}"
+    respond_to do |format|
+      if @match.update(match_params)
+        format.html { redirect_to matches_url, notice: 'Match was successfully updated.' }
+        format.json { render :show, status: :created, location: @match }
+      else
+        format.html { render :edit }
+        format.json { render json: @match.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  # # Use callbacks to share common setup or constraints between actions.
+  # def set_match
+  #   @match = Match.find(params[:id])
+  # end
+
+  # Only allow a list of trusted parameters through.
+    def match_params
+      puts "Entered match_params method"
+      puts "---------------------------------------------o-o-o-o-o---o-o-o-o-o- params are #{params}"
+      match_params = params.require(:match).permit(:user, :matched_user, :status)
+      match_params[:user] = User.find(match_params[:user].to_i)
+      match_params[:matched_user] = User.find(match_params[:matched_user].to_i)
+      return match_params
+    end
+
+
 end
