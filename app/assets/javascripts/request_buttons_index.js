72298314c6f79bf027df9e4d64b3dedfb428212d
antu <katehuen@gmail.com>
2020-06-16 22:03:49 +0200

    some minor changes, trying to have ajax working on heroku

--- app/assets/javascripts/request_buttons_index.js
+++ app/assets/javascripts/request_buttons_index.js
@@ -1,26 +1,26 @@
-$(document).ready(function() {
-
-  // $("#emails-list").children().click(function(){
-  $("div[id*='request-button-match']").on('click','form', function() {
-    // let match_id = event.target.id.slice(21);
-    let match_id = event.target.parentNode.id.slice(21);
-    console.log(`div botton for match ${match_id}`);
-
-    $.ajax({
-    type:'PATCH',
-    url: `/matches/${match_id}`,
-    // data: { match: match_id },
-    // data: { match: { user: parseInt(match_id)}} ,
-    dataType: "script",
-    success: function(result){
-      console.log(`match updated! success!!!`);
-    }
-    });
-
-
-  });
-
-
-
-// LAST CLOSURE
-});
+// $(document).ready(function() {
+//
+//   // $("#emails-list").children().click(function(){
+//   $("div[id*='request-button-match']").on('click','form', function() {
+//     // let match_id = event.target.id.slice(21);
+//     let match_id = event.target.parentNode.id.slice(21);
+//     console.log(`div botton for match ${match_id}`);
+//
+//     $.ajax({
+//     type:'PATCH',
+//     url: `/matches/${match_id}`,
+//     // data: { match: match_id },
+//     // data: { match: { user: parseInt(match_id)}} ,
+//     dataType: "script",
+//     success: function(result){
+//       console.log(`match updated! success!!!`);
+//     }
+//     });
+//
+//
+//   });
+//
+//
+//
+// // LAST CLOSURE
+// });
