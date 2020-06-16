4c5bc7057b01fa5151566673673d53d3aeedafed
antu <katehuen@gmail.com>
2020-06-16 19:13:51 +0200

    precompiled assets, trying to fix ajax on Heroku

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -59,7 +59,7 @@ class MatchesController < ApplicationController
       if @match.update(match_params)
         format.html { redirect_to matches_url, notice: 'Request was sent succesfully.' }
         format.json { render :show, status: :created, location: @match }
-        format.js { }
+        format.js { puts "======== respond_to detected js format!"}
       else
         format.html { render :edit }
         format.json { render json: @match.errors, status: :unprocessable_entity }
